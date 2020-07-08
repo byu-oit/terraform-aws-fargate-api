23c96a8e8ef09174344d7e9754842912fa23e11d
yoshutch <yoshutch@gmail.com>
2020-07-08 15:25:14 -0600

    updated version to 2.3.0

--- examples/simple/simple.tf
+++ examples/simple/simple.tf
@@ -11,7 +11,7 @@ module "acs" {
 //  name = "fake-example-cluster"
 //}
 module "fargate_api" {
-  source = "github.com/byu-oit/terraform-aws-fargate-api?ref=v2.2.0"
+  source = "github.com/byu-oit/terraform-aws-fargate-api?ref=v2.3.0"
   //  source           = "../../" // for local testing
   app_name = "example-api"
   //  ecs_cluster_name = aws_ecs_cluster.existing.name
