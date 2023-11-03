7f0d02a3b1e3f6eec8032f36b65c7ab7b7a1aea0
Jamie Visker <jvisker@byu.edu>
2023-11-03 14:41:00 -0600

    fix parameter

--- main.tf
+++ main.tf
@@ -591,7 +591,7 @@ resource "aws_appautoscaling_policy" "default" {
     predefined_metric_specification {
       predefined_metric_type = var.autoscaling_config.metric_type
     }
-    target_value       = var.autoscaling_config.cpu_percentage_target
+    target_value       = var.autoscaling_config.target_value
     scale_in_cooldown  = var.autoscaling_config.scale_in_cooldown
     scale_out_cooldown = var.autoscaling_config.scale_out_cooldown
   }
